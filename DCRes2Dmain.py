83e9d0af3a79facf0b09d4041f84e279fe4219f8
BenKary <ben@BensLaptop.(none)>
2013-05-22 16:18:34 -0700

    All operators are working - fingers crossed

--- DCRes2Dmain.py
+++ DCRes2Dmain.py
@@ -7,92 +7,117 @@ Created on Sat May 11 17:43:51 2013
 
 import numpy as np
 import scipy.sparse as ssp
+import ModOps as mo
 
 # Set up a mesh
-nx = 21.
-ny = 11.
-
-# def some functions to be used on each dimension
+nx = 21.  ; nxc = nx - 1
+ny = 11.  ; nyc = ny - 1
 
 n2c = lambda(n): n[0:-1] + 0.5*np.diff(n)
-e = lambda(n): np.ones([1, n-1])
-
+e   = lambda(n): np.ones([1, n])
+    
 def dnc(n):
+    
     dnc = np.diff(n); 
     dnc = np.insert(dnc, 0, dnc[0]/2.);  # correct endpoints for a half step between
     dnc = np.append(dnc, dnc[-1]/2.)     # first/last cell center and the boundary
+    
     return(dnc)
+
     
 def neumann_diags(m,n):
-    diags = np.vstack([-e(n)*1./m[1:], e(n)*1./m[:-1]])
-    diags[1, 0] = diags[1, 0]*2; diags[0, -1] = diags[0, -1]*2
-    return(diags)
+    
+    d = np.vstack([-e(n-1)*1./m[1:], e(n-1)*1./m[:-1]])
+    d[1, 0] = d[1, 0]*2; d[0, -1] = d[0, -1]*2
+    
+    return(d)
+    
+def AV_diags(m):
+    
+    d = np.vstack([mo.e(m-1), mo.e(m-1)])
+    d[1, 0] = d[1, 0]*2; d[0, -1] = d[0, -1]*2
+    
+    return d
+    
 
+    
 # Create field and flux position vectors
-x   = np.linspace(0,1,nx);
+x   = np.linspace(0,1,nx)
 xc  = n2c(x)
-dx  = np.diff(x)
 dxc = dnc(xc)
 
-
-y   = np.linspace(0,1,ny);
+y   = np.linspace(0,1,ny)
 yc  = n2c(y)
-dy  = np.diff(y)
 dyc = dnc(yc)
 
+Dc2nx = ssp.spdiags(neumann_diags(dxc,nx),[-1,0],nx,nxc)
+Dc2ny = ssp.spdiags(neumann_diags(dyc,ny),[-1,0],ny,nyc)
 
-# Unused so far
-X  , Y  = np.meshgrid(x, y)
-Xc , Yc = np.meshgrid(xc, yc)
+Gcx = ssp.kron(ssp.eye(nxc,nxc), Dc2ny)
+Gcy = ssp.kron(Dc2nx, ssp.eye(nyc,nyc))
 
-Dc2nx = ssp.spdiags(neumann_diags(dxc,nx),[-1,0],nx,nx-1);
-Dc2ny = ssp.spdiags(neumann_diags(dyc,ny),[-1,0],ny,ny-1);
-Dn2cx = ssp.spdiags(np.vstack([-e(nx), e(nx)]),[0,1],nx-1,nx);
-Dn2cy = ssp.spdiags(np.vstack([-e(ny), e(ny)]),[0,1],ny-1,ny);
+GRAD = ssp.vstack([Gcx, Gcy])
 
-Icy = ssp.eye(ny,ny-1)
-Icx = ssp.eye(nx,nx-1)
+    
+# Create field and flux position vectors
+x   = np.linspace(0,1,nx)
+y   = np.linspace(0,1,ny)
+dx  = np.diff(x)
+dy  = np.diff(y)
 
-Gcx = ssp.kron(Icx,Dc2ny)
-Gcy = ssp.kron(Dc2nx,Icy)
-
-Iny = ssp.eye(ny-1,ny)
-Inx = ssp.eye(nx-1,nx)
-
-Gnx = ssp.kron(Inx,Dn2cy)
-Gny = ssp.kron(Dn2cx,Iny)
+Dn2cx = ssp.spdiags(np.vstack([-e(nxc), e(nxc)]),[0,1],nxc,nx)
+Dn2cy = ssp.spdiags(np.vstack([-e(nyc), e(nyc)]),[0,1],nyc,ny)
 
 
-GRAD = ssp.vstack([Gcx, Gcy]);
-DIV  = ssp.hstack([Gnx, Gny]);
+Dnx = ssp.kron(ssp.eye(nxc,nxc), Dn2cy)
+Dny = ssp.kron(Dn2cx, ssp.eye(nyc,nyc))
+
+DIV  = ssp.hstack([Dnx, Dny])
+
+    
+#GRAD = mo.getGRAD (nx , ny)
+#DIV  = mo.getDIV  (nx , ny)
 
 # Some code to check that the GRAD, DIV operators work properly
+longdim = ny*nxc + nyc*nx
 
-Sigma = np.ones(np.shape(Xc))
+Sigma = np.ones((nyc,nxc))
 Sigma[5:8, 8:14] = 1000.
 Sigma = np.ravel(Sigma, order='F'); Sigma = Sigma.T
 
-#GRADsig = GRAD * Sigma
-#GRADsigx = np.reshape(GRADsig[0:nx*ny], np.shape(X), order='F')
-#GRADsigy = np.reshape(GRADsig[nx*ny-1:-1], np.shape(Y), order='F')
+GRADsig = GRAD * Sigma
+GRADsigy = np.reshape(GRADsig[0 : nxc*ny], (ny, nxc), order='F')
+GRADsigx = np.reshape(GRADsig[ny*nxc : longdim], (nyc, nx), order='F')
 
-#LAPsig = DIV * GRADsig
-#LAPsig = np.reshape(LAPsig, np.shape(Xc), order='F')
-
-
-diagsx = np.vstack([e(nx), e(nx)])
-diagsx[1, 0] = diagsx[1, 0]*2; diagsx[0, -1] = diagsx[0, -1]*2
-
-diagsy = np.vstack([e(ny), e(ny)])
-diagsy[1, 0] = diagsy[1, 0]*2; diagsy[0, -1] = diagsy[0, -1]*2
+diagsx = AV_diags(nx)
+diagsy = AV_diags(ny)
 
 
-Ac2fx = ssp.spdiags(diagsx,[-1, 0],nx,nx-1)
-Ac2fy = ssp.spdiags(diagsy,[-1, 0],ny,ny-1)
+Ac2fx = 1./2 * ssp.spdiags(diagsx,[-1, 0],nx,nxc)
+Ac2fy = 1./2 * ssp.spdiags(diagsy,[-1, 0],ny,nyc)
 
-ll = ssp.kron(ssp.eye(nx-1,nx-1), Ac2fy); ll = ll.todense()
-mm = ssp.kron(Ac2fx, ssp.eye(ny,ny)); mm = mm.todense()
-nn = mm * ll
+#AVy = ssp.kron(ssp.eye(nx,nx), Ac2fy); AVy = AVy.todense()
+#AVx = ssp.kron(Ac2fx, ssp.eye(ny-1,ny-1)); AVx = AVx.todense()
+#AV  = np.dot(AVy, AVx)
 
-AVsig = 1. / (1./2 * np.dot(nn,(1./Sigma)))
-AVsig = np.reshape(AVsig, np.shape(X), order='F')
+AVy = ssp.kron(ssp.eye(nxc,nxc), Ac2fy); #AVy = AVy.todense()
+AVx = ssp.kron(Ac2fx, ssp.eye(nyc,nyc)); #AVx = AVx.todense()
+#AV  = ssp.vstack([AVx,AVy])
+
+
+#AVsig = 1. / (np.dot(AV,(1./Sigma)))
+#AVsig = np.reshape(AVsig, np.shape(X), order='F')
+
+
+ll = 1./(np.dot(AVy.todense(),1./Sigma)); ll = np.ravel(ll, order='F')
+
+#ll = np.reshape(ll, [ny, nxc], order='F')
+
+mm = 1./(np.dot(AVx.todense(),1./Sigma)); mm = np.ravel(mm, order='F')
+
+#mm = np.reshape(mm, [nyc, nx], order='F')
+
+diagAVsig = ssp.spdiags(np.append(ll,mm),0,longdim,longdim)
+
+Aofu = np.dot(DIV, np.dot(diagAVsig, GRAD))
+
