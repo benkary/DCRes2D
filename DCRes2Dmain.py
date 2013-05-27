d4315b1051c5f8d3ae6d60869b642801ae4f4b14
BenKary <ben@BensLaptop.(none)>
2013-05-26 21:33:19 -0700

    initial meanderings

--- DCRes2Dmain.py
+++ DCRes2Dmain.py
@@ -8,6 +8,7 @@ Created on Sat May 11 17:43:51 2013
 import numpy as np
 import numpy.linalg as nl
 import scipy.sparse as ssp
+import scipy.optimize as sop
 import ModOps as mo
 
 # Set up a mesh
@@ -23,65 +24,9 @@ y   = np.linspace(0,1,ny)
 dx  = np.diff(x)
 dy  = np.diff(y)
     
-#def dnc(n):
-#    
-#    dnc = np.diff(n); 
-#    dnc = np.insert(dnc, 0, dnc[0]/2.);  # correct endpoints for a half step between
-#    dnc = np.append(dnc, dnc[-1]/2.)     # first/last cell center and the boundary
-#    
-#    return(dnc)
-#
-#    
-#def neumann_diags(m,n):
-#    
-#    d = np.vstack([-e(n-1)*1./m[1:], e(n-1)*1./m[:-1]])
-#    d[1, 0] = d[1, 0]*2; d[0, -1] = d[0, -1]*2
-#    
-#    return(d)
-#    
-#def AV_diags(m):
-#    
-#    d = np.vstack([mo.e(m-1), mo.e(m-1)])
-#    d[1, 0] = d[1, 0]*2; d[0, -1] = d[0, -1]*2
-#    
-#    return d
-#    
-#
-#    
-## Create field and flux position vectors
-#x   = np.linspace(0,1,nx)
-#xc  = n2c(x)
-#dxc = dnc(xc)
-#
-#y   = np.linspace(0,1,ny)
-#yc  = n2c(y)
-#dyc = dnc(yc)
-#
-#Dc2nx = ssp.spdiags(neumann_diags(dxc,nx),[-1,0],nx,nxc)
-#Dc2ny = ssp.spdiags(neumann_diags(dyc,ny),[-1,0],ny,nyc)
-#
-#Gcx = ssp.kron(ssp.eye(nxc,nxc), Dc2ny)
-#Gcy = ssp.kron(Dc2nx, ssp.eye(nyc,nyc))
-#
-#GRAD = ssp.vstack([Gcx, Gcy])
-#
-#    
-
-#
-#Dn2cx = ssp.spdiags(np.vstack([-e(nxc), e(nxc)]),[0,1],nxc,nx)
-#Dn2cy = ssp.spdiags(np.vstack([-e(nyc), e(nyc)]),[0,1],nyc,ny)
-#
-#
-#Dnx = ssp.kron(ssp.eye(nxc,nxc), Dn2cy)
-#Dny = ssp.kron(Dn2cx, ssp.eye(nyc,nyc))
-#
-#DIV  = ssp.hstack([Dnx, Dny])
-#
-#    
-
-
-GRAD = mo.getGRAD (nx , ny)
-DIV  = mo.getDIV  (nx , ny)
+## Create the GRAD and DIV operators to be used
+GRAD = mo.getGRAD (x , y)
+DIV  = mo.getDIV  (x , y)
 
 # Some code to check that the GRAD, DIV operators work properly
 longdim = ny*nxc + nyc*nx
@@ -94,55 +39,39 @@ GRADsig = GRAD * Sigma
 GRADsigy = np.reshape(GRADsig[0 : nxc*ny], (ny, nxc), order='F')
 GRADsigx = np.reshape(GRADsig[ny*nxc : longdim], (nyc, nx), order='F')
 
-#LAPsig = np.dot(DIV, GRADsig)
-#diagsx = AV_diags(nx)
-#diagsy = AV_diags(ny)
-#
-#
-#Ac2fx = 1./2 * ssp.spdiags(diagsx,[-1, 0],nx,nxc)
-#Ac2fy = 1./2 * ssp.spdiags(diagsy,[-1, 0],ny,nyc)
-#
-##AVy = ssp.kron(ssp.eye(nx,nx), Ac2fy); AVy = AVy.todense()
-##AVx = ssp.kron(Ac2fx, ssp.eye(ny-1,ny-1)); AVx = AVx.todense()
-##AV  = np.dot(AVy, AVx)
-#
-#AVy = ssp.kron(ssp.eye(nxc,nxc), Ac2fy); #AVy = AVy.todense()
-#AVx = ssp.kron(Ac2fx, ssp.eye(nyc,nyc)); #AVx = AVx.todense()
-##AV  = ssp.vstack([AVx,AVy])
-#
-#
-##AVsig = 1. / (np.dot(AV,(1./Sigma)))
-##AVsig = np.reshape(AVsig, np.shape(X), order='F')
+## Create the xy center to node averaging matrices apply them to Sigma and
+#  diagonalize the results to interact with the DIV operator on the left
+#  and the GRAD on the right
+##
 
 AVx, AVy = mo.getAV(nx, ny)
 
+AVsigx = 1./(np.dot(AVx.todense(),1./Sigma)); AVsigx = np.ravel(AVsigx, order='F')
+AVsigy = 1./(np.dot(AVy.todense(),1./Sigma)); AVsigy = np.ravel(AVsigy, order='F')
+diagAVsig = ssp.spdiags(np.append(AVsigx,AVsigy),0,longdim,longdim)
 
-ll = 1./(np.dot(AVy.todense(),1./Sigma)); ll = np.ravel(ll, order='F')
-
-#ll = np.reshape(ll, [ny, nxc], order='F')
-
-mm = 1./(np.dot(AVx.todense(),1./Sigma)); mm = np.ravel(mm, order='F')
-
-#mm = np.reshape(mm, [nyc, nx], order='F')
-
-#diagAVsig = ssp.block_diag(ll,mm)
-
-diagAVsig = ssp.spdiags(np.append(ll,mm),0,longdim,longdim)
+## Create a matrix to pad the matrix inversion and satisfy the condition e.Ty = 0
 
 DX, DY = np.meshgrid(dx, dy)
 h = DX * DY; h = np.ravel(h, order='F')
 hmat = np.tile(h, (nxc*nyc, 1))
 
-#hmat = mo.get_hmat(dx,dy)
-
-
-Aofu = np.dot(DIV.todense(), np.dot(diagAVsig, GRAD).todense()) + hmat
+## Test the forward operation with a test source
 
 q = np.zeros((nyc, nxc)); q[0,1] = 5; q = np.ravel(q, order='F')
-V = nl.solve(Aofu,q)
+V = nl.solve(DIV * diagAVsig * GRAD + hmat, q)
 
 V = np.reshape(V,(nyc,nxc), order='F')
 
 
+## Need a projection operator that interpolates the solution to the forward
+#  problem to the locations of the inversion input data - can maybe hold off on
+#  this
+##
+
+## Create the Jacobian for use in Gradient-based optimization routines
+Vd = V[0,:]
+
+Jac = DIV * ssp.diags(GRAD * np.ravel(V, order='F'), 0) * diagAVsig**2 * 
 
 
