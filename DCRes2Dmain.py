3e782026f03af8a7983009137c3102b6781f4779
BenKary <ben@BensLaptop.(none)>
2013-05-29 05:10:05 -0700

    began interpolation matrix

--- DCRes2Dmain.py
+++ DCRes2Dmain.py
@@ -9,6 +9,8 @@ import numpy as np
 import numpy.linalg as nl
 import scipy.sparse as ssp
 import scipy.optimize as sop
+import scipy.interpolate as si
+
 import ModOps as mo
 
 # Set up a mesh
@@ -46,8 +48,10 @@ GRADsigx = np.reshape(GRADsig[ny*nxc : longdim], (nyc, nx), order='F')
 
 AVx, AVy = mo.getAV(nx, ny)
 
-AVsigx = 1./(np.dot(AVx.todense(),1./Sigma)); AVsigx = np.ravel(AVsigx, order='F')
-AVsigy = 1./(np.dot(AVy.todense(),1./Sigma)); AVsigy = np.ravel(AVsigy, order='F')
+AVsigx = 1./(np.dot(AVx.todense(),1./Sigma)); 
+AVsigx = np.ravel(AVsigx, order='F')
+AVsigy = 1./(np.dot(AVy.todense(),1./Sigma)); 
+AVsigy = np.ravel(AVsigy, order='F')
 diagAVsig = ssp.spdiags(np.append(AVsigx,AVsigy),0,longdim,longdim)
 
 ## Create a matrix to pad the matrix inversion and satisfy the condition e.Ty = 0
@@ -62,16 +66,40 @@ q = np.zeros((nyc, nxc)); q[0,1] = 5; q = np.ravel(q, order='F')
 V = nl.solve(DIV * diagAVsig * GRAD + hmat, q)
 
 V = np.reshape(V,(nyc,nxc), order='F')
+V_noisy = V + 0.05 * np.random.randn(nyc,nxc)
 
 
+
+
+## Create the Jacobian for use in Gradient-based optimization routines
+
+
+AVsigx = 1./(np.dot(AVx.todense(),1./Sigma)); 
+AVsigx = np.ravel(AVsigx, order='F')
+AVsigy = 1./(np.dot(AVy.todense(),1./Sigma)); 
+AVsigy = np.ravel(AVsigy, order='F')
+diagAVsig = ssp.spdiags(np.append(AVsigx,AVsigy),0,longdim,longdim)
+
+G = (DIV * ssp.diags(GRAD * np.ravel(V, order='F'), 0) * diagAVsig**2 *
+      ssp.vstack((AVx, AVy)) * ssp.diags(Sigma, 0))
+      
+#def fun():
+#     
+#sop.minimize()
+#
+      
 ## Need a projection operator that interpolates the solution to the forward
 #  problem to the locations of the inversion input data - can maybe hold off on
 #  this
 ##
+      
+xdat = x[13:21]
+ydat = np.zeros((np.shape(xdat)))
 
-## Create the Jacobian for use in Gradient-based optimization routines
-Vd = V[0,:]
-
-Jac = DIV * ssp.diags(GRAD * np.ravel(V, order='F'), 0) * diagAVsig**2 * 
-
-
+xc  = mo.n2c(x)
+yc  = mo.n2c(y)
+Xc, Yc = meshgrid(xc,yc)
+Q = (si.griddata((np.ravel(Xc, order='F'), np.ravel(Yc, order='F')), 
+    np.eye(nyc*nxc), (xdat,ydat), method='linear', fill_value='0'))
+    
+Vdat = Q * np.ravel(V, order='F')
