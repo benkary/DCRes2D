99d2ed720bc457405c63daf3c36316fb78e43000
BenKary <ben@BensLaptop.(none)>
2013-06-21 12:21:58 -0700

    not sure what the status is here

--- DCRes2Dmain.py
+++ DCRes2Dmain.py
@@ -10,6 +10,7 @@ import numpy.linalg as nl
 import scipy.sparse as ssp
 import scipy.optimize as sop
 import scipy.interpolate as si
+import pylab as p
 
 import ModOps as mo
 
@@ -34,7 +35,7 @@ DIV  = mo.getDIV  (x , y)
 longdim = ny*nxc + nyc*nx
 
 Sigma = 1./40000* np.ones((nyc,nxc))
-Sigma[7:10, 14:18] = 1./3000.
+Sigma[7:10, 11:14] = 1./3000.
 Sigma = np.ravel(Sigma, order='F'); Sigma = Sigma.T
 
 GRADsig = GRAD * Sigma
@@ -48,7 +49,7 @@ GRADsigx = np.reshape(GRADsig[ny*nxc : longdim], (nyc, nx), order='F')
 
 AVx, AVy = mo.getAV(nx, ny)
 
-AVsigx = 1./(np.dot(AVx.todense(),1./Sigma)); 
+AVsigx = 1./(np.dot(AVx.todense(),1./Sigma)); 5
 AVsigx = np.ravel(AVsigx, order='F')
 AVsigy = 1./(np.dot(AVy.todense(),1./Sigma)); 
 AVsigy = np.ravel(AVsigy, order='F')
@@ -62,11 +63,12 @@ hmat = np.tile(h, (nxc*nyc, 1))
 
 ## Test the forward operation with a test source
 
-q = np.zeros((nyc, nxc)); q[0,1] = 5; q = np.ravel(q, order='F')
+q = np.zeros((nyc, nxc)); q[0,8] = 5; q[0,17] = -5; q = np.ravel(q, order='F')
 V = nl.solve(DIV * diagAVsig * GRAD + hmat, q)
 
 V = np.reshape(V,(nyc,nxc), order='F')
 V_noisy = V + 0.05 * np.random.randn(nyc,nxc)
+
 
 
 
@@ -80,9 +82,9 @@ AVsigy = 1./(np.dot(AVy.todense(),1./Sigma));
 AVsigy = np.ravel(AVsigy, order='F')
 diagAVsig = ssp.spdiags(np.append(AVsigx,AVsigy),0,longdim,longdim)
 
-G = (DIV * ssp.diags(GRAD * np.ravel(V, order='F'), 0) * diagAVsig**2 *
-      ssp.vstack((AVx, AVy)) * ssp.diags(Sigma, 0))
-      
+
+
+
 #def fun():
 #     
 #sop.minimize()
@@ -92,41 +94,67 @@ G = (DIV * ssp.diags(GRAD * np.ravel(V, order='F'), 0) * diagAVsig**2 *
 #  problem to the locations of the inversion input data - can maybe hold off on
 #  this
 ##
-      
-xdat = xc[13:20]
-ydat = np.zeros((np.shape(xdat)))
 
 xc  = mo.n2c(x)
 yc  = mo.n2c(y)
 Xc, Yc = meshgrid(xc,yc)
-#Q = (si.griddata((np.ravel(Xc, order='F'), np.ravel(Yc, order='F')), 
-#    np.eye(nyc*nxc), (xdat,ydat), method='linear', fill_value='0'))
+ 
 
-#interpx = np.interp(xdat,xc,np.ones(np.shape(xc)))
-#blah = interpx * np.ravel(V, order='F')
-   
-   
+xdat = xc[13:20]
+ydat = np.zeros((np.shape(xdat)))
+
+
+
+  
 #Vdat = Q * np.ravel(V, order='F')
-
-diagsQ, a, Q = mo.get_Q(xdat, xc)
-
-#n = 0
-#ll = np.zeros(np.shape(xdat))
-#mm = np.zeros(np.shape(xdat))
 #
-#for i in range(0, np.size(xc)):
-#   
-#   if xc[i] > xdat[n]:
-#      
-#      xdif  = xc[i] - xdat[n]
-#      ll[n] = xdif / np.diff(xc)[i - 1]
-#      mm[n] = 1 - ll[n]
-#      n = n + 1
-#   
-#   elif xc[i] == xdat[n]:
-#       
-#      ll[n] = 1
-#      mm[n] = 0
-#      n = n + 1
-      
-     
+## code to test the interpolation operator
+#
+#t  = np.array([0.1,0.2,0.3,0.4,0.5])
+#ft = np.exp(t)*np.sin(pi*t)
+#t_interp = np.array([0.12,0.21,0.28,0.35])
+#
+#tt1 = np.interp(t_interp,t,ft)
+#Qtx, Qty = mo.get_Q(t_interp, t)
+#tt2 = np.dot(Qt,ft)
+#
+#p.plot(t,ft)
+#p.show()
+#
+
+
+## Goin for it
+
+# Will want to re-implement these using the matrix vector formulation in ha0
+# paper
+
+# define an initial model
+#m0 = 1./40000* np.ones((nyc,nxc))
+#m0 = np.ravel(m0, order='F'); m0 = m0.T
+#
+#Beta = 1
+#
+#AVsigx = 1./(np.dot(AVx.todense(),1./m0)); 
+#AVsigx = np.ravel(AVsigx, order='F')
+#AVsigy = 1./(np.dot(AVy.todense(),1./m0)); 
+#AVsigy = np.ravel(AVsigy, order='F')
+#diagAVsig = ssp.spdiags(np.append(AVsigx,AVsigy),0,longdim,longdim)
+#    
+#A = DIV * diagAVsig * GRAD + hmat  
+#
+#G = (DIV * ssp.diags(GRAD * np.ravel(V, order='F'), 0) * diagAVsig**2 *
+#      ssp.vstack((AVx, AVy)) * ssp.diags(m0, 0))
+#
+Qx, Qy = mo.get_Q(np.array([xdat, ydat]), np.array([xc, yc]))
+#prV = Q * np.ravel(V_noisy, order='F')
+#
+#
+#J = np.dot(-Q, np.dot(np.solve(A,G)))
+#
+#Rm  = DIV * GRAD * m0
+#Rmm = DIV * GRAD
+#
+#Hred = J.T * J + Beta * Rmm
+#p = Beta * Rm + J.T * (Q * np.solve(A,q) - Vnoisy)
+#
+#dm = np.solve()
